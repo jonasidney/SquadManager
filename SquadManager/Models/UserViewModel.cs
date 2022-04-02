2bfb97f98db114c22d420e51ededb4aa50944d86
Jonas Sidney <jonasidney@hotmail.com>
2022-04-01 22:00:41 -0300

    Add project files.

---
+++ SquadManager/Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SquadManager.Models
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        
+    }
+}
