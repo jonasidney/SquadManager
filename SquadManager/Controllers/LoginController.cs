b0fbbaa67e3680dba055d527eda300e39edd8cb4
Jonas Sidney <jonasidney@hotmail.com>
2022-04-03 11:55:43 -0300

    Criação e implementação da Classe User Validator (Aula 04 - Mod 01)

--- SquadManager/Controllers/LoginController.cs
+++ SquadManager/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using SquadManager.Models;
+using SquadManager.Validator;
 
 namespace SquadManager.Controllers
 {
@@ -8,7 +10,19 @@ namespace SquadManager.Controllers
         public IActionResult Index()
         {
             UserViewModel user = new UserViewModel();
-            
+
+            UserValidator validator = new UserValidator();
+
+            ValidationResult results = validator.Validate(user);
+
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+            }
+
             return View("Index", user);
         }
 
---
+++ SquadManager/Validator/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SquadManager.Models;
+
+namespace SquadManager.Validator
+{
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Email).NotNull().WithMessage("Campo e-mail nao pode ser vazio");
+            RuleFor(user => user.Email).EmailAddress().WithMessage("Digite um e-mail valido");
+            RuleFor(user => user.Password).NotNull().WithMessage("Campo senha nao pode ser vazio");
+        }
+    }
+}
